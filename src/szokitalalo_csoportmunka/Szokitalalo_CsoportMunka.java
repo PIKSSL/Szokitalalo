f1f9ea5f856b5c53e35792b00b5f743733a72e8e
PIKSSL <purpledog120@gmail.com>
2022-03-22 20:56:05 +0100

    alap

---
+++ src/szokitalalo_csoportmunka/Szokitalalo_CsoportMunka.java
@@ -0,0 +1,32 @@
+package szokitalalo_csoportmunka;
+public class Szokitalalo_CsoportMunka {
+
+    public static void main(String[] args) {
+        
+        start();
+        
+        
+    }
+
+    static void start(){
+        String[] tarolo = {"Ló","Tó","Őz"};
+    }
+    
+    static String beker(){
+
+        return null;
+
+    }
+    static boolean visszajelzes(String szoveg){
+        
+        return false;
+    }
+    static void osszehasonlitas(String szoveg){
+        
+    }
+    static void kisNagy(String szoveg){
+        
+    }
+    
+    
+}   
