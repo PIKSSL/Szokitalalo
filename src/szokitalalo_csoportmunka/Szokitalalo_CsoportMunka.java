cb888abc8b6ad710a3699330499bac709348a37c
PIKSSL <purpledog120@gmail.com>
2022-03-22 22:32:34 +0100

    osszehasonlitasv1

--- src/szokitalalo_csoportmunka/Szokitalalo_CsoportMunka.java
+++ src/szokitalalo_csoportmunka/Szokitalalo_CsoportMunka.java
@@ -2,14 +2,26 @@ package szokitalalo_csoportmunka;
 public class Szokitalalo_CsoportMunka {
 
     public static void main(String[] args) {
-        
+        System.out.println("asd");
         start();
-        
-        
+      
     }
 
     static void start(){
-        String[] tarolo = {"Ló","Tó","Őz"};
+        //TESZT
+//        String[] tarolo = {"Ló","Tó","Őz"};
+//        String szo1 = "Ló";
+//        String szo2 = "Tó";
+//        System.out.println("asd");
+//        int szam = osszehasonlitas(szo1, szo2);
+//        if(szam == 10){
+//            System.out.println("Megegyezik a szó");
+//        }else if(szam == 11){
+//            System.out.println("Teljesen különböznek");
+//        }
+//        else{
+//            System.out.println("A "+szo2.charAt(szam)+" megyezik");
+//        } //TESZT
     }
     
     static String beker(){
@@ -21,12 +33,23 @@ public class Szokitalalo_CsoportMunka {
         
         return false;
     }
-    static void osszehasonlitas(String szoveg){
-        
+    static int osszehasonlitas(String szo1, String szo2){
+        int i = 0;
+        if(szo1 == szo2){
+            return 10;
+        }else{
+            while(i < szo2.length()){
+                char kk = szo2.charAt(i);
+                String k = ""+kk;
+                if (szo1.contains(k)){
+                    return i;
+                }
+            i++;
+            }
+        }
+        return 11;
     }
     static void kisNagy(String szoveg){
         
     }
-    
-    
 }   
