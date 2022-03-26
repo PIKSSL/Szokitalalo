6524c289c1d34013f95882039cc0de4b97d436d6
PIKSSL <purpledog120@gmail.com>
2022-03-26 02:11:35 +0100

    hibak_nincskesz

--- src/szokitalalo_csoportmunka/Szokitalalo_CsoportMunka.java
+++ src/szokitalalo_csoportmunka/Szokitalalo_CsoportMunka.java
@@ -1,6 +1,9 @@
 package szokitalalo_csoportmunka;
 
+import java.text.Normalizer;
+import java.util.Random;
 import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
 
 public class Szokitalalo_CsoportMunka {
 
@@ -11,7 +14,17 @@ public class Szokitalalo_CsoportMunka {
     }
 
     static void start(){
-        String[] tarolo = {"Ló","Tó","Őz"};
+       Random rnd = new Random();
+       int veletlenSzo=rnd.nextInt(3);
+       int i = 0;
+       String[] tarolo = {"Ló","Tó","Őz"};
+       String szo1=kisNagy(tarolo[veletlenSzo]) ;
+       String szo2=kisNagy(beker("Adja meg a tippjét!\n>>"));
+       System.out.println(szo1+" "+ szo2);
+       System.out.println(visszajelzes(osszehasonlitas(szo1, szo2), szo1, szo2));
+            
+       {
+       }
           //TESZT OSSZEHASONLITAS
 //        String szo1 = "Ló";
 //        String szo2 = "Tó";
@@ -31,15 +44,33 @@ public class Szokitalalo_CsoportMunka {
             //TESZT KIS BETŰ KONVERTALAS
     }
     
-    static String beker(){
+    static String beker(String szoveg){
         Scanner scn = new Scanner(System.in);
+        System.out.print(szoveg);
         String tipp = scn.nextLine();
+           
         return tipp;
 
     }
-    static String visszajelzes(int eredmeny){
+    static String visszajelzes(int eredmeny, String szo1, String szo2){
+        String kiir="";
+        if (eredmeny==10){
+             kiir="Eltalálta a szót!";
+            }
+        else if  (eredmeny == 11){
+              kiir="A két szó nem egyezik!";
+        }
+        else{
+           if(szo1.charAt(eredmeny) == szo2.charAt(eredmeny)){
+               kiir="A "+szo2.charAt(eredmeny)+" a megfelelő helyen található.";
+           }else{
+               kiir="A "+szo2.charAt(eredmeny)+" benne van a szóban, de nem a megfelelő helyen.";
+           }
+            
+//            kiir="A"+eredmeny+"."+" helyen álló betü helyes";
+        }
+       return kiir;
         
-        return "";
     }
     static int osszehasonlitas(String szo1, String szo2){
         int i = 0;
@@ -57,6 +88,21 @@ public class Szokitalalo_CsoportMunka {
         }
         return 11;
     }
+    
+//    static boolean ellenorzes(String adat, int tipus){
+//        if(adat.equals("")){
+//            System.out.println("HIBA:üres mező");
+//            return false;
+//        }
+//        if(tipus == 1){
+//            for (int i = 0; i < adat.length(); i++)
+//            if (!Character.isDigit(adat.charAt(i))){
+//                System.out.println("HIBA:csak számokat adhat meg");
+//                return false;}
+//        }
+//        
+//        return true;
+//    }
     static String kisNagy(String szoveg){
         String kisbetu = szoveg.toLowerCase();
         return kisbetu;
