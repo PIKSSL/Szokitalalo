638f94cdc5a796ac10956335998624e1dbbc3ed7
PIKSSL <purpledog120@gmail.com>
2022-03-27 01:24:44 +0100

    javitott_PIKSS

--- src/szokitalalo_csoportmunka/Szokitalalo_CsoportMunka.java
+++ src/szokitalalo_csoportmunka/Szokitalalo_CsoportMunka.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
 
 public class Szokitalalo_CsoportMunka {
-
+       static Random rnd = new Random();
+       static int veletlenSzo=rnd.nextInt(3);
     public static void main(String[] args) {
         
         start();
@@ -14,15 +15,21 @@ public class Szokitalalo_CsoportMunka {
     }
 
     static void start(){
-       Random rnd = new Random();
-       int veletlenSzo=rnd.nextInt(3);
-       int i = 0;
+
+
+       boolean k = true;
+       while(k){
        String[] tarolo = {"Ló","Tó","Őz"};
        String szo1=kisNagy(tarolo[veletlenSzo]) ;
        String szo2=kisNagy(beker("Adja meg a tippjét!\n>>"));
        System.out.println(szo1+" "+ szo2);
        System.out.println(visszajelzes(osszehasonlitas(szo1, szo2), szo1, szo2));
-            
+       String valasz = beker("Szeretné újra megpróbálni?\n[1]Igen\n[2]Nem\n>>"); //Az egyszerüség kedvéért tételezzük fel hogy a felhasználó ezen a ponton számot fog megadni az adott opciók közül.
+       if(Integer.parseInt(valasz) == 2){
+           System.out.println("Viszlát!");
+           k = false;
+       }
+       }
        {
        }
           //TESZT OSSZEHASONLITAS
@@ -45,7 +52,7 @@ public class Szokitalalo_CsoportMunka {
     }
     
     static String beker(String szoveg){
-        Scanner scn = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in, "ISO-8859-1");
         System.out.print(szoveg);
         String tipp = scn.nextLine();
            
@@ -56,31 +63,47 @@ public class Szokitalalo_CsoportMunka {
         String kiir="";
         if (eredmeny==10){
              kiir="Eltalálta a szót!";
+             veletlenSzo=rnd.nextInt(3);
             }
         else if  (eredmeny == 11){
               kiir="A két szó nem egyezik!";
         }
+        else if (eredmeny == 12){
+              kiir="A két betű helyes, de rossz helyen vannak!";}
         else{
-           if(szo1.charAt(eredmeny) == szo2.charAt(eredmeny)){
+             if(szo1.charAt(eredmeny) == szo2.charAt(eredmeny)){
                kiir="A "+szo2.charAt(eredmeny)+" a megfelelő helyen található.";
            }else{
                kiir="A "+szo2.charAt(eredmeny)+" benne van a szóban, de nem a megfelelő helyen.";
-           }
+           }}
+        
+            
+//            for (int i = 0; i < szo1.length(); i++) {
+//
+//                String szo_2 = ""+szo2.charAt(eredmeny);
+//           if(szo1.contains(szo_2) && szo1.charAt(eredmeny) == szo2.charAt(eredmeny)){
+//               kiir="A "+szo2.charAt(eredmeny)+" a megfelelő helyen található.";
+//           }else{
+//               kiir="A "+szo2.charAt(eredmeny)+" benne van a szóban, de nem a megfelelő helyen.";
+//           }}
             
 //            kiir="A"+eredmeny+"."+" helyen álló betü helyes";
-        }
+//        }
        return kiir;
         
     }
     static int osszehasonlitas(String szo1, String szo2){
         int i = 0;
-        if(szo1 == szo2){
+        if(szo1.contains(szo2)){
             return 10;
         }else{
             while(i < szo2.length()){
-                char kk = szo2.charAt(i);
-                String k = ""+kk;
-                if (szo1.contains(k)){
+                String k = ""+szo2.charAt(i);
+                String l = ""+szo2.charAt(0);
+                String ll = ""+szo2.charAt(1);
+                if (szo1.contains(l) && szo1.contains(ll)){
+                    return 12;
+                }else if(szo1.contains(k)){
                     return i;
                 }
             i++;
@@ -105,6 +128,8 @@ public class Szokitalalo_CsoportMunka {
 //    }
     static String kisNagy(String szoveg){
         String kisbetu = szoveg.toLowerCase();
-        return kisbetu;
+                String ki = StringUtils.replace(kisbetu, "õ", "ő");
+                ki = StringUtils.replace(ki, "û", "ű");
+        return ki;
     }
 }   
